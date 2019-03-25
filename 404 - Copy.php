b85d013bf1f8ed29ce6957597031f70a592eaeb1
Md.Omar Faruq <43860406+faruqiAhmed@users.noreply.github.com>
2019-03-25 22:23:30 +0600

    Add files via upload

---
+++ 404 - Copy.php
@@ -0,0 +1,46 @@
+<?php 
+include "inc/header.php";
+?>
+
+<body>
+
+<div class="page_loader"></div>
+
+<!-- Option Panel -->
+<div class="option-panel option-panel-collased">
+    <h2>Change Color</h2>
+    <div class="color-plate default-plate" data-color="default"></div>
+    <div class="color-plate blue-plate" data-color="blue"></div>
+    <div class="color-plate yellow-plate" data-color="yellow"></div>
+    <div class="color-plate red-plate" data-color="red"></div>
+    <div class="color-plate green-light-plate" data-color="green-light"></div>
+    <div class="color-plate orange-plate" data-color="orange"></div>
+    <div class="color-plate yellow-light-plate" data-color="yellow-light"></div>
+    <div class="color-plate green-light-2-plate" data-color="green-light-2"></div>
+    <div class="color-plate olive-plate" data-color="olive"></div>
+    <div class="color-plate purple-plate" data-color="purple"></div>
+    <div class="color-plate blue-light-plate" data-color="blue-light"></div>
+    <div class="color-plate brown-plate" data-color="brown"></div>
+    <div class="setting-button">
+        <i class="fa fa-gear"></i>
+    </div>
+</div>
+<!-- /Option Panel -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <!-- Error404 content start -->
+            <div class="error404-content">
+                <h1>404</h1>
+                <h2>Something is wrong</h2>
+                <p>The page you are looking for was moved, removed, renamed or might never existed.</p>
+                <form action="https://themevessel-items-dot-themeforest-171208.appspot.com/the-nest/index.html" method="post">
+                    <button type="submit" class="button-md button-theme">Go Home</button>
+                </form>
+            </div>
+            <!-- Error404 content end -->
+        </div>
+    </div>
+</div>
+<?php include "inc/footer.php"; ?>
